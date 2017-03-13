a45cd77e7b80e14eba87f7fc0b621d4455b3523c
kyokomi <kyoko1220adword@gmail.com>
2017-03-13 19:59:46 +0900

    Fix chdir

--- run.sh
+++ run.sh
@@ -14,9 +14,12 @@ fi
 go get -u github.com/golang/dep/...
 
 if [ "${VERSION}" != "" ]; then
+    CURRENT_DIR=$(pwd)
     cd $GOPATH/src/github.com/golang/dep/cmd/dep
     git checkout ${VERSION}
     go install github.com/golang/dep/cmd/dep
+    git branch
+    cd $CURRENT_DIR
 fi
 
 dep ensure ${OPT}
